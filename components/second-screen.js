b6e728c3d78a03a7281377638ceb4c51a2a13ab6
Florian DURRIEU <f.durrieux@meetic-corp.com>
2016-10-13 19:51:01 +0200

    no background adpatation

--- components/second-screen.js
+++ components/second-screen.js
@@ -12,9 +12,13 @@ export default class SecondScreen extends Component {
     super(props);
   }
   render() {
+    cover = <View style={{width: 400, height: 200, backgroundColor: '#AA00AA'}} />;
+    if (this.props.member.cover!==undefined) {
+      cover = <Image source={{uri: this.props.member.cover.source}} style={{width: 400, height: 200}} />;
+    };
     return (
       <View style={this.props.styles.container}>
-        <Image source={{uri: this.props.member.cover.source}} style={{width: 400, height: 200}} />
+        {cover}
         <Image source={{uri: this.props.member.picture.data.url}} style={{width: 80, height: 80, marginTop: -30}} />
         <Text>{this.props.member.first_name}</Text>
         <Text>{JSON.stringify(this.props.member.friends.data)}</Text>
