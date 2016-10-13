1eb74926940f21712f925bd8fdbd3db4a57f457a
c.verdier <c.verdier.ext@meetic-corp.com>
2016-10-13 22:01:35 +0200

    Friends list with pictures (ugly mode)

--- components/second-screen.js
+++ components/second-screen.js
@@ -4,13 +4,26 @@ import {
   Text,
   View,
   ActivityIndicator,
-  Image
+  Image,
+  ListView
 } from 'react-native';
 
 export default class SecondScreen extends Component {
   constructor(props) {
     super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      friendsList: ds.cloneWithRows(props.member.friends)
+    };
   }
+
+  renderFriend(friend) {
+  	return	(<View style={{flex: 1, flexDirection: 'row', width: 360, height: 120, backgroundColor: '#AAFFFF'}}>
+        		<Image source={{uri: friend.picture.data.url}} style={{width: 100, height: 100}} />
+        		<Text>{friend.name}</Text>
+        	</View>);
+  }
+
   render() {
     cover = <View style={{width: 400, height: 200, backgroundColor: '#AA00AA'}} />;
     if (this.props.member.cover!==undefined) {
@@ -21,7 +34,7 @@ export default class SecondScreen extends Component {
         {cover}
         <Image source={{uri: this.props.member.picture.data.url}} style={{width: 80, height: 80, marginTop: -30}} />
         <Text>{this.props.member.first_name}</Text>
-        <Text>{JSON.stringify(this.props.member.friends.data)}</Text>
+        <ListView dataSource={this.state.friendsList} renderRow={ (friend) => this.renderFriend(friend)} />
       </Image>
     );
   }
