3e9805e23b53156cbef26074793ebd2765178dcb
c.verdier <c.verdier.ext@meetic-corp.com>
2016-10-13 20:12:45 +0200

    Add BG

--- components/second-screen.js
+++ components/second-screen.js
@@ -17,12 +17,12 @@ export default class SecondScreen extends Component {
       cover = <Image source={{uri: this.props.member.cover.source}} style={{width: 400, height: 200}} />;
     };
     return (
-      <View style={this.props.styles.container}>
+      <Image source={require('../assets/bg.png')} style={{flex: 1, alignItems: 'center', width: 360, height: 640}}>
         {cover}
         <Image source={{uri: this.props.member.picture.data.url}} style={{width: 80, height: 80, marginTop: -30}} />
         <Text>{this.props.member.first_name}</Text>
         <Text>{JSON.stringify(this.props.member.friends.data)}</Text>
-      </View>
+      </Image>
     );
   }
 }
